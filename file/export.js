3c2d36fa691994f26b7982e58a9d059c5f7bee19
mzrdev <87870017+MZRCode@users.noreply.github.com>
2024-08-05 22:56:42 +0300

    Update export.js

--- file/export.js
+++ file/export.js
@@ -4,13 +4,13 @@ const { version } = require('../package.json');
 const { __exportStar } = require('tslib');
 const axios = require('axios');
 
-// versionCheck(version);
+versionCheck(version);
 
 __exportStar(require('./index'), exports);
 
 exports.version = version;
 
-/*function versionCheck(version) {
+function versionCheck(version) {
     try {
         axios.get('https://registry.npmjs.org/mzr-api/latest').then((response) => {
             const data = response.data;
@@ -18,4 +18,4 @@ exports.version = version;
             if (version !== data.version) console.warn(`[MZR API] It seems like you are using an outdated version of mzr-api. Run 'npm install mzr-api@${data.version}' to update the module. Need help? Join my Discord server: https://discord.gg/ktVdQYrtXF`);
         });
     } catch (err) { }
-}*/
+}
