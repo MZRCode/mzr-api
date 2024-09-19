50c8c188eb28228ed1208a84424224fe72e42395
mzrdev <87870017+MZRCode@users.noreply.github.com>
2024-09-19 15:45:02 +0300

    Add files via upload

--- file/index.js
+++ file/index.js
@@ -1,7 +1,8 @@
 const { handleError } = require('../utils/handleError');
-const baseULR = 'https://api.mzrdev.xyz/v1';
+const baseURL = 'https://api.mzrdev.xyz';
 const website = 'https://www.mzrdev.xyz';
 const axios = require('axios');
+const apiVersion = 'v1';
 
 class Api {
     constructor(apiKey) {
@@ -12,7 +13,7 @@ class Api {
 
     async altin() {
         try {
-            const api = await axios.get(`${baseULR}/altin`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/altin`, {
                 headers: {
                     'content-type': 'application/json',
                     'Authorization': this.apiKey
@@ -31,7 +32,7 @@ class Api {
         if (currencies && !Array.isArray(currencies)) throw new Error(`[MZR API - CRYPTO] The 'currencies' field is required to be an array, but the provided value is not an array. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/crypto`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/crypto`, {
                 params: { coins, currencies },
                 headers: {
                     'content-type': 'application/json',
@@ -50,7 +51,7 @@ class Api {
         if (parseInt(limit) > 500) throw new Error(`[MZR API - DEPREMLER] The 'limit' field must be a number and this number must not be greater than 500. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/depremler`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/depremler`, {
                 params: { limit },
                 headers: {
                     'content-type': 'application/json',
@@ -67,7 +68,7 @@ class Api {
 
     async doviz() {
         try {
-            const api = await axios.get(`${baseULR}/doviz`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/doviz`, {
                 headers: {
                     'content-type': 'application/json',
                     'Authorization': this.apiKey
@@ -88,7 +89,7 @@ class Api {
         if (!image || image === '' || typeof image !== 'string') throw new Error(`[MZR API - FUN] The 'image' parameter is missing. Please provide the text to be translated in the 'image' query parameter. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/fun`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/fun`, {
                 params: { type, image },
                 headers: {
                     'content-type': 'application/json',
@@ -110,7 +111,7 @@ class Api {
         if (parseInt(limit) > 20) throw new Error(`[MZR API - HABERLER] The 'limit' field must be a number and this number must not be greater than 500. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/haberler?kategori=${kategori}&limit=${limit}`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/haberler?kategori=${kategori}&limit=${limit}`, {
                 headers: {
                     'content-type': 'application/json',
                     'Authorization': this.apiKey
@@ -128,7 +129,7 @@ class Api {
         if (!ip || ip === '' || typeof ip !== 'string') throw new Error(`[MZR API - MINECRAFT SERVER] The 'ip' parameter is missing. Please type the target minecraft server ip in the 'ip' query parameter. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/minecraftServer?ip=${ip}&port=${port}`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/minecraftServer?ip=${ip}&port=${port}`, {
                 headers: {
                     'content-type': 'application/json',
                     'Authorization': this.apiKey
@@ -147,7 +148,7 @@ class Api {
         if (!ilce || ilce === '' || typeof ilce !== 'string') ilce = il;
 
         try {
-            const api = await axios.get(`${baseULR}/namazVakitleri`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/namazVakitleri`, {
                 params: { il, ilce },
                 headers: {
                     'content-type': 'application/json',
@@ -166,7 +167,7 @@ class Api {
         if (!game || game === '' || typeof game !== 'string') throw new Error(`[MZR API - STEAM] Need to type a game name in the 'Game' field, but you didn't. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/steam`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/steam`, {
                 params: { game },
                 headers: {
                     'content-type': 'application/json',
@@ -186,7 +187,7 @@ class Api {
         if (!to || to === '' || typeof translate !== 'string') throw new Error(`[MZR API - TRANSLATE] The 'to' parameter is missing. Please provide the target language code in the 'to' query parameter. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/translate`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/translate`, {
                 params: { message, to, autoCorrect },
                 headers: {
                     'content-type': 'application/json',
@@ -205,7 +206,7 @@ class Api {
         if (!id || id === '' || typeof id !== 'string') throw new Error(`[MZR API - USERS] The 'id' parameter is missing. Please enter the discord user ID in the 'id' query parameter. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/users/${id}`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/users/${id}`, {
                 headers: {
                     'content-type': 'application/json',
                     'Authorization': this.apiKey
@@ -223,7 +224,7 @@ class Api {
         if (!search || search === '' || typeof search !== 'string') throw new Error(`[MZR API - WIKIPEDIA] The 'search' parameter is missing. Please type the target wikipedia search content in the 'search' query parameter. Need help? Go to ${website} and read what is written.`);
 
         try {
-            const api = await axios.get(`${baseULR}/wikipedia`, {
+            const api = await axios.get(`${baseURL}/${apiVersion}/wikipedia`, {
                 params: { search },
                 headers: {
                     'content-type': 'application/json',
@@ -235,6 +236,67 @@ class Api {
             else handleError(api.data);
         } catch (e) {
             throw new Error(e.message);
+        }
+    }
+
+    async proxyCheck(proxies) {
+        if (!proxies || !Array.isArray(proxies)) throw new Error(`[MZR API - PROXYCHECK] The 'proxies' field is required to be an array, but the provided value is not an array. Need help? Go to ${website} and read what is written.`);
+
+        try {
+            const api = await axios.get(`${baseURL}/${apiVersion}/proxycheck`, {
+                params: { proxies },
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': this.apiKey
+                },
+            });
+
+            if (api?.data?.success) return api.data.data;
+            else handleError(api.data);
+        } catch (e) {
+            throw new Error(e.message);
+        }
+    }
+
+    async rateLimitCheck(proxies, url) {
+        if (!proxies || !Array.isArray(proxies)) throw new Error(`[MZR API - RATELIMITCHECK] The 'proxies' field is required to be an array, but the provided value is not an array. Need help? Go to ${website} and read what is written.`);
+        if (!url || !url.startsWith('https://')) throw new Error(`[MZR API - RATELIMITCHECK] The parameter 'url' is missing or not valid. Please provide a valid URL starting with 'https://'. Need help? Go to ${website} and read what is written.`);
+
+        let rateLimitedProxies = [];
+        let notWorkingProxies = [];
+        let workingProxies = [];
+
+        for (const proxy of proxies) {
+            try {
+                let response = { success: false, rateLimited: false, working: false };
+
+                try {
+                    response = await axios.get(`${baseURL}/${apiVersion}/ratelimitcheck`, {
+                        params: { proxies: [proxy], url },
+                        headers: {
+                            'content-type': 'application/json',
+                            'Authorization': this.apiKey
+                        }
+                    });
+                } catch { }
+
+                if (response.data.rateLimited) rateLimitedProxies.push(proxy);
+                else if (response.data.working) workingProxies.push(proxy);
+                else notWorkingProxies.push(proxy);
+            } catch (e) {
+                notWorkingProxies.push(proxy);
+            };
+        }
+
+        return {
+            stats: {
+                rateLimitedProxies: rateLimitedProxies.length || 0,
+                notWorkingProxies: notWorkingProxies.length || 0,
+                workingProxies: workingProxies.length || 0
+            },
+            rateLimitedProxies,
+            notWorkingProxies,
+            workingProxies
         }
     }
 }
